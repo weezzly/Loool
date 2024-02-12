168b12cd40e488e5d877caeb2b36b33de4ac47ab
weezzly <weassly123@gmail.com>
2024-02-12 14:59:19 +0800

    new commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
+
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //Movement
+        _moveVector = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        // Fall and jump
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
