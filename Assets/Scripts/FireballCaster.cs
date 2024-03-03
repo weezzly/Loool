e628bc3bc5c4046865bbacfd81568bdd9e3fcfde
weezzly <weassly123@gmail.com>
2024-03-03 17:30:31 +0800

    New commit2

---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
